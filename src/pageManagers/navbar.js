d99a7e8b181a1ff454370edd53ee2fa4f3903673
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-14 18:26:42 -0500

    working on hamburger changed class of ul in nav bar

--- src/pageManagers/navbar.js
+++ src/pageManagers/navbar.js
@@ -64,10 +64,11 @@ class Navbar extends PageManager{
          <a class="navbar-brand" href="#">🧹 AntiProcrastanator</a>
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
            <span class="navbar-toggler-icon"></span>
+           
          </button>
        
          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-           <ul class="navbar-nav mr-auto">
+           <ul class="nav navbar-nav">
              <li class="nav-item active">
                <a class="nav-link" id="welcome-link" href="#">Welcome <span class="sr-only">(current)</span></a>
              </li>
