bbb1889c87448f01e2fda35391172a605ee9e256
blackwellsmith <blackwellsmithiv@gmail.com>
2020-02-14 20:05:11 -0500

    new navbar

--- src/pageManagers/navbar.js
+++ src/pageManagers/navbar.js
@@ -34,29 +34,30 @@ class Navbar extends PageManager{
         if (this.is_authenticated) {
             return (`
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">🧹 AntiProcrastinator</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+              <a class="navbar-brand" href="#">🧹 AntiProcrastinator</a>
+              <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+              aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
+                  class="fas fa-bars fa-1x"></i></span></button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" id="profile-link" href="#">Profile <span class="sr-only">(current)</span></a>
-      </li>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" id="profile-link" href="#">Profile <span class="sr-only">(current)</span></a>
+                    </li>
       
-      <li class="nav-item">
-        <a class="nav-link" id="cleaning-link" href="#">Cleaning Tips</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" id="life-link" href="#">Enjoying Life</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" id="logout-link" href="#">Logout </a>
-      </li>
-    </ul> 
-  </div>
-</nav>
+                    <li class="nav-item">
+                      <a class="nav-link" id="cleaning-link" href="#">Cleaning Tips</a>
+                    </li>
+      
+                    <li class="nav-item">
+                      <a class="nav-link" id="life-link" href="#">Enjoying Life</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="logout-link" href="#">Logout </a>
+                    </li>
+                  </ul> 
+                </div>
+            </nav>
             `)
         } else {
          return   (`
